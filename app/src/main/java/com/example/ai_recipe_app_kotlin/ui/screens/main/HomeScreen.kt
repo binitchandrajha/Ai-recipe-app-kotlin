c77adeb4bce11da2249c4d3f2b315fb2738cee8d
Binit Chandra Jha <binit.jha@outcodesoftware.com>
2026-05-26 23:03:23 +0545

    make the whole homescreen scrollable

--- app/src/main/java/com/example/ai_recipe_app_kotlin/ui/screens/main/HomeScreen.kt
+++ app/src/main/java/com/example/ai_recipe_app_kotlin/ui/screens/main/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.ai_recipe_app_kotlin.ui.screens.main
 
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,11 +16,12 @@ import com.example.ai_recipe_app_kotlin.ui.theme.PrimaryColor
 
 @Composable
 fun HomeScreen(){
+    val scrollState = rememberScrollState()
     Scaffold(
         containerColor = PrimaryColor
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier.padding(innerPadding).verticalScroll(scrollState)
         ){
             ProfileHeader()
             IngredientSearchCard()
